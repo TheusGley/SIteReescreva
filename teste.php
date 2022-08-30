181d4396784bd43e81f937310d5caece1dd0e90e
TheusGley <81973986+TheusGley@users.noreply.github.com>
2022-08-30 18:48:49 -0300

    Add files via upload
    30/08

---
+++ teste.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
+
+    <link rel="stylesheet" type="text/css" href="custom.css">
+</head>
+
+<body>
+  
+    <div class="">
+        <div class="">
+            <h2 class="titulo" style="margin-top: 10% ;">Redações disponiveis </h2>
+
+           
+        </div>
+
+        <span id="msgAlerta"></span>
+        <div >
+            <div>
+                <div>
+                    
+                    
+                    <table  class= "reda-box" >
+                        <thead>
+                            <tr>
+                                <th>  </th>
+                                <th>Nome</th>
+                                <th>Email</th>
+                                <th>Status</th>
+                                
+                            </tr>
+                            
+                        </thead>
+                            <tbody>
+
+                            </tbody>
+                    </table>
+                    
+                    
+                </div>
+
+
+
+
+                <div>
+                <h3 class="titulo" style="float:right"> Redações corrigidas</h3>
+                </div>
+                <div class="div3">
+
+                
+                <div>
+               
+                    
+                  
+                    <table  class= "">
+                        <thead> 
+                           
+                            <tr>
+                                <th>  </th>
+                                <th>Nome</th>
+                                <th>Email</th>
+                                <th>Status</th>
+                                
+                            </tr>
+                            
+                        </thead>
+                            <tbody>
+
+                            </tbody>
+                    </table>
+                   
+                  
+               
+                    
+                </div>
+
+            
+                        
+                     
+            </div>
+
+        </div>      
+                
+                
+            </body>
+
+            </html>
