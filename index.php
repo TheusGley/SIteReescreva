9ad586d1e93c41d1d3c23db92f1920e4a1570f24
TheusGley <81973986+TheusGley@users.noreply.github.com>
2022-08-30 12:07:43 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Re-Escreva</title>
+
+    <link rel="stylesheet" type="text/css" href="custom.css">
+
+</head>
+<body>
+
+    <div class="container" > 
+    <div class="box">
+    <form action="post" id="form1">
+    <div class="borda">
+    <h1 class="titulo" > Login </h1>
+    </div>
+    <label class="font"> Nome de usuario</label>
+    <br>
+    <input  type="login" required > 
+    <br>
+    <br>
+    <label class="font"> Senha </label>
+    <br>
+    <input   type="password" required>
+    
+<button  type="submit" form="form1" value="Submit">Entrar</button>
+</div>
+    </div>
+    </form>
+    
+</body>
+</html>
