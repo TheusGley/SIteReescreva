6b55daa972ca75d4ee2a45ae6880bbdc6711a173
TheusGley <81973986+TheusGley@users.noreply.github.com>
2022-09-05 19:01:05 -0300

    Update conexaobd.php
    05/09

--- conexaobd.php
+++ conexaobd.php
@@ -6,6 +6,11 @@ $pass = "";
 $dbname = "db_redacao";
 $port = 3306;
 
+
+$conect = mysqli_connect($host, $user, $pass, $dbname);
+
+mysqli_select_db($conect,"DB_redacao");
+
 try{
     //Conexão com a porta
     $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
@@ -14,6 +19,7 @@ try{
     $conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
 
     echo "Conexão com banco de dados realizado com sucesso!";
+
 }  catch(PDOException $err){
     echo "Erro: Conexão com banco de dados não foi realizada com sucesso. Erro gerado " . $err->getMessage();
 }
